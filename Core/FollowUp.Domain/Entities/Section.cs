cb6819e46375babd55dba3ecbf85a088f2a158c6
selim.bayindir <selim.bayindir@SELIM-BAYINDIR>
2023-02-09 16:20:24 +0300

    Add project files.

---
+++ Core/FollowUp.Domain/Entities/Section.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FollowUp.Domain.Entities
+{
+    public class Section
+    {
+        public int Id { get; set; }
+        public string SectionName { get; set; }
+    }
+}
